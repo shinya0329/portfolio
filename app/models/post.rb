18f3beea61dcd738805c6d9077f1562fbd938cc4
EC2 Default User <ec2-user@ip-172-31-26-162.ap-northeast-1.compute.internal>
2025-04-07 03:55:11 +0000

    いいね機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,12 @@
 class Post < ApplicationRecord
    has_one_attached :image
    
-   belongs_to :user 
+   belongs_to :user
    has_many :post_comments, dependent: :destroy
+   has_many :favorites, dependent: :destroy
+   
+   def favorited_by?(user)
+      favorites.where(user_id: user.id).exists?
+   end
+   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
          
   has_many :posts, dependent: :destroy 
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 end
