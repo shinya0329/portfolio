cd8247f2a5e13a6af8fd6ee7c50c6e950d973705
EC2 Default User <ec2-user@ip-172-31-26-162.ap-northeast-1.compute.internal>
2025-04-10 07:51:14 +0000

    category一覧、category詳細,about画面の追加

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -10,7 +10,8 @@ class PostCommentsController < ApplicationController
   end
   
   def destroy
-    
+    Comment.find_by(id: params[:id]).destroy
+    redirect_to post_path(params[:post_id])
   end
   
   private
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,13 +16,51 @@ class PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.page(params[:page]).reverse_order
+    @posts = Post.page(params[:page]).reverse_order(10)
+  end
+
+  def categorys
+    
+  end
+  
+  def category
+     @category = params[:category] #categoryを取得
+    if @category == "JapaneseFood"
+      @posts = Post.where(category:"1").page(params[:page]).reverse_order.per(10)
+    elsif @category == "WesternCuisine"
+      @posts = Post.where(category:"2").page(params[:page]).reverse_order.per(10)
+    elsif @category == "ChineseFood"
+      @posts = Post.where(category:"3").page(params[:page]).reverse_order.per(10)
+    elsif @category == "AsianFood"
+      @posts = Post.where(category:"4").page(params[:page]).reverse_order.per(10)
+    elsif @category == "Curry"
+      @posts = Post.where(category:"5").page(params[:page]).reverse_order.per(10)
+    elsif @category == "Meat"
+      @posts = Post.where(category:"6").page(params[:page]).reverse_order.per(10)
+    elsif @category == "Drink"
+      @posts = Post.where(category:"7").page(params[:page]).reverse_order.per(10)
+    elsif @category == "Cafe"
+      @posts = Post.where(category:"8").page(params[:page]).reverse_order.per(10)
+    elsif @category == "Sweets"
+      @posts = Post.where(category:"9").page(params[:page]).reverse_order.per(10)
+    elsif @category == "Noodles"
+      @posts = Post.where(category:"10").page(params[:page]).reverse_order.per(10)
+    elsif @category == "Bar"
+      @posts = Post.where(category:"11").page(params[:page]).reverse_order.per(10)
+    elsif @category == "Buffet"
+      @posts = Post.where(category:"12").page(params[:page]).reverse_order.per(10)
+    elsif @category == "Family"
+      @posts = Post.where(category:"13").page(params[:page]).reverse_order.per(10)
+    elsif @category == "Restaurant"
+      @posts = Post.where(category:"14").page(params[:page]).reverse_order.per(10)
+    end
   end
 
   def show
     @post = Post.find(params[:id])
     @post_comment = PostComment.new
   end
+
 
   def destroy
     @post = Post.find(params[:id])
